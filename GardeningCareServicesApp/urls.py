3e7fee7285f5882909c2c4932ced25b61b527f2a
DelyanNikolov <145500850+DelyanNikolov@users.noreply.github.com>
2024-12-11 21:37:08 +0200

    chage media urls to work with debug true

--- GardeningCareServicesApp/urls.py
+++ GardeningCareServicesApp/urls.py
@@ -17,8 +17,8 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,5 +29,6 @@ urlpatterns = [
     path('homeowners/', include('GardeningCareServicesApp.homeowners.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
